4f28c4b017623d1c3a0f4d0b38eb6fb4dd33a837
Dragan Pepic <d.pepic@hotmail.com>
2020-07-14 18:22:31 +0200

    dgd

---
+++ TemporalneOperacije/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemporalneOperacije
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DateTime tackaUvremenu = DateTime.Now;
+			Console.WriteLine("Sada je: " + tackaUvremenu.ToString("ddd mmmm HH:MM"));
+
+			TimeSpan periodVremena = new TimeSpan(7, 12, 0, 0);
+			DateTime buducnost = tackaUvremenu + periodVremena;
+			Console.WriteLine("Neka buducnost:) : " + buducnost);
+			Console.WriteLine("Buducnost minus sada: " + (buducnost - tackaUvremenu));
+			Console.ReadKey();
+		}
+	}
+}
